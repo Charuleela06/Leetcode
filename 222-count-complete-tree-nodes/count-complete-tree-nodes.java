bf3c33c342452468f994dbd3955722cb370d5264
Charuleela06 <155134642+Charuleela06@users.noreply.github.com>
2026-01-29 15:44:41 +0530

    Time: 0 ms (100.00%) | Memory: 49.7 MB (22.22%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+        public int countNodes(TreeNode root) {
+                if (root == null) {
+                            return 0;
+                                    }
+
+                                            int leftHeight = getHeight(root.left);
+                                                    int rightHeight = getHeight(root.right);
+
+                                                            // If left and right subtree heights are equal
+                                                                    if (leftHeight == rightHeight) {
+                                                                                // Left subtree is perfect
+                                                                                            return (1 << leftHeight) + countNodes(root.right);
+                                                                                                    } else {
+                                                                                                                // Right subtree is perfect
+                                                                                                                            return (1 << rightHeight) + countNodes(root.left);
+                                                                                                                                    }
+                                                                                                                                        }
+
+                                                                                                                                            private int getHeight(TreeNode node) {
+                                                                                                                                                    int height = 0;
+                                                                                                                                                            while (node != null) {
+                                                                                                                                                                        height++;
+                                                                                                                                                                                    node = node.left;
+                                                                                                                                                                                            }
+                                                                                                                                                                                                    return height;
+                                                                                                                                                                                                                                       }                                                                                           }
+                                                                                                                                                                                                    
