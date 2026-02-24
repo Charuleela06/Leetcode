714fb4a57572c016ab02d791511d0ea4cb413b64
Charuleela06 <155134642+Charuleela06@users.noreply.github.com>
2026-02-24 22:10:21 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (28.76%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.java
@@ -14,24 +14,24 @@
  * }
  */
 class Solution {
-        public int sumRootToLeaf(TreeNode root) {
-                return dfs(root, 0);
-                    }
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
 
-                        private int dfs(TreeNode node, int currentValue) {
-                                if (node == null)
-                                            return 0;
+    private int dfs(TreeNode node, int currentValue) {
+        if (node == null)
+            return 0;
 
-                                                    // Update current binary value
-                                                            currentValue = (currentValue << 1) | node.val;
+        // Update current binary value
+        currentValue = (currentValue << 1) | node.val;
 
-                                                                    // If leaf node, return computed value
-                                                                            if (node.left == null && node.right == null) {
-                                                                                        return currentValue;
-                                                                                                }
+        // If leaf node, return computed value
+        if (node.left == null && node.right == null) {
+            return currentValue;
+        }
 
-                                                                                                        // Otherwise, sum from left and right
-                                                                                                                return dfs(node.left, currentValue) +
-                                                                                                                               dfs(node.right, currentValue);
-                                                                                                                                   }
-                                                                                                                                   }
+        // Otherwise, sum from left and right
+        return dfs(node.left, currentValue) +
+                dfs(node.right, currentValue);
+    }
+}
